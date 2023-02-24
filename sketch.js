e345ddc2d6fdbaafc2d384348a984fa9b4c1d661
Caio <57820884+caio-mbarbosa@users.noreply.github.com>
2023-02-24 00:28:35 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-let mapa;
-
 function setup() {
-  mapa = new Map(800, 600, 18, 18);
+  createCanvas(800, 600);
+  background(50);
+  var mapa = new Map(800, 600, 18, 18);
 }
 
 function draw() {
-  mapa.show();
+  //mapa.show();
 }
