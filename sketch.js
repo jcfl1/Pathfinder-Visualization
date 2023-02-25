3f6148dfde8f262349fe03eab5497d4ee54e7275
Júlio César Farias <73369019+jcfl1@users.noreply.github.com>
2023-02-25 19:19:41 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,40 @@
+var mapa;
+
+var INCREMENTAL_BFS = true;
+var hasFinished = false;
+var hasArrived = false;
+var count = 0;
+
+
 function setup() {
+  frameRate(60);
   createCanvas(800, 600);
   background(50);
-  var mapa = new Map(800, 600, 18, 18);
+  mapa = new Map(800, 600, 18, 18);
+  
+  if(INCREMENTAL_BFS){
+    mapa.initialize_visited();
+    mapa.setup_incremental_bfs();
+  }
 }
 
 function draw() {
-  //mapa.show();
+  
+  if(mapa.agent_pos_x == mapa.target_pos_x && mapa.agent_pos_y == mapa.target_pos_y){
+    hasArrived = true;
+    noLoop();
+  }
+  
+  if(mapa.visited[mapa.target_pos_y][mapa.target_pos_x] && !hasArrived){
+    hasFinished = true;
+    
+    mapa.walk(count);
+    count = count+1;
+  }
+  
+  if(INCREMENTAL_BFS && !hasFinished){
+    mapa.incremental_bfs();
+  }
+  
+  mapa.show();
 }
